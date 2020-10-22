e2f9de0b49721a43295cb08f08ed8743461ca027
Gusna Ikhsan <gusna.ikhsan7@gmail.com>
2020-10-22 23:23:54 +0700

    tse css

--- application/controllers/BerandaController.php
+++ application/controllers/BerandaController.php
@@ -2,25 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class BerandaController extends CI_Controller {
-
-	// function __construct()
-	// {
-	// 	parent::__construct();
-	// 	// if ($this->session->userdata('logged_in') !== TRUE) {
-	// 	//   redirect('login');
-	// 	// }
-	// 	$this->load->model('Model_Berita');
-	// 	date_default_timezone_set('Asia/Bangkok');
-	// }
-
 	public function index(){
-
-		// print_r($this->model_berita->totalBerita(4));
 		$data['news'] = $this->model_berita->lastNews(4);
 		$data['announc'] = $this->model_berita->lastAnnouncement(3);
 		$data['headline'] = $this->model_berita->headLine(3);
 		$data['agenda'] = $this->model_agenda->lastAgenda(3);
-        $this->load->view('global_css');
+        // $this->load->view('global_css');
         $this->load->view('header_mobile');
         $this->load->view('header');
 		$this->load->view('beranda', $data);
--- application/views/beranda.php
+++ application/views/beranda.php
@@ -1,5 +1,24 @@
 <!DOCTYPE html>
 <html lang="en-US" class="no-js">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Pengairan Universitas Brawijaya" />
+    <meta name="description" content="Pengairan Universitas Brawijaya">
+    <meta name="author" content="filkom">
+
+    <title>Teknik Pengairan Universitas Brawijaya</title>
+    <link rel="icon" href="<?php echo base_url('assets/logo/ubicon.ico'); ?>">
+
+    <link rel='stylesheet' href="<?php echo base_url('asset/plugins/goodlayers-core/plugins/combine/style.css'); ?>" type='text/css' media='all' />
+    <link rel='stylesheet' href="<?php echo base_url('asset/plugins/goodlayers-core/include/css/page-builder.css'); ?>" type='text/css' media='all' />
+    <link rel='stylesheet' href="<?php echo base_url('asset/plugins/revslider/public/assets/css/settings.css'); ?>" type='text/css' media='all' />
+    <link rel='stylesheet' href="<?php echo base_url('asset/css/style-core.css'); ?>" type='text/css' media='all' />
+    <link rel='stylesheet' href="<?php echo base_url('asset/css/kingster-style-custom.css'); ?>" type='text/css' media='all' />
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700%2C400" rel="stylesheet" property="stylesheet" type="text/css" media="all">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Poppins%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2Cregular%2Citalic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CABeeZee%3Aregular%2Citalic&amp;subset=latin%2Clatin-ext%2Cdevanagari&amp;ver=5.0.3' type='text/css' media='all' />
+</head>
 <body class="home page-template-default page page-id-2039 gdlr-core-body woocommerce-no-js tribe-no-js kingster-body kingster-body-front kingster-full  kingster-with-sticky-navigation  kingster-blockquote-style-1 gdlr-core-link-to-lightbox">
     <!-- header mobile navigation -->
     <div class="kingster-body-outer-wrapper ">
